62a7e16858801ff7d286384bb5c7ef95c9fa6769
Albert Penticoss <a.penticoss@latrobe.edu.au>
2019-08-29 11:47:17 +1000

    Update banner-box video

--- js/banner-video.js
+++ js/banner-video.js
@@ -1,24 +1,26 @@
 $('.banner-box-ctas').on('click', '#banner-video-play, #banner-video-play-mobile', function(e) {
-        e.preventDefault();
-        var target = e.target
-        var videoId = $(target).attr('data-youtube');
-        var overlay = $('#vid-' + videoId);
-        var closeOverlay = $('#banner-overlay-close');
+    e.preventDefault();
+    var target = e.target
+    var videoId = $(target).attr('data-youtube');
+    var overlay = $('.banner-overlay');
+    var closeOverlay = $('#banner-overlay-close');
 
-        closeOverlay.on('click keydown', function(event) {
-            if (event.keyCode === undefined || event.keyCode == 13 || event.keyCode == 27) {
-                event.preventDefault();
-                overlay.fadeOut(function(){
-                    // stop the video
-                    $('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
-                    target.focus();
-                });
-            }
-        })
+    // handler for overlay close button
+    closeOverlay.on('click keydown', function(event) {
+        if (event.keyCode === undefined || event.keyCode == 13 || event.keyCode == 27) {
+            // undefined for click, 13 for enter key, 27 for esc key
+            event.preventDefault();
+            overlay.fadeOut(function(){
+                // delete video iframe to stop playing
+                $('#vid-'+videoId+').remove();
+                target.focus();
+            });
+        }
+    })
 
-        overlay.fadeIn(function(){
-            $(closeOverlay).focus();
-            //play video
-            $('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
-        });    
+    overlay.fadeIn(function(){
+        $(closeOverlay).focus();
+        // inject video with autoplay
+        $('.banner-video-wrapper .yt-video').append('<iframe id="vid-'+videoId+'" class="youtube-video" width="560" height="315" type="text/html" src="https://www.youtube.com/embed/'+videoId+'?rel=0&amp;autoplay=1" frameborder="0" allowfullscreen></iframe>');
+    });
 });
