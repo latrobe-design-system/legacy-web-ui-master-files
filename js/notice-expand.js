db468e64d4c80e6bc6a25616fe564c69e35b2e5e
Albert Penticoss <a.penticoss@latrobe.edu.au>
2020-04-07 10:57:56 +1000

    Notice updates for read more

---
+++ js/notice-expand.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    // expand control click handler for 
+    $('.expand-control').click(function() {
+
+      var regionId = $(this).attr('aria-controls');
+
+      if ($(this).attr('aria-expanded') == 'false') { // region is collapsed
+
+          // update the aria-expanded attribute of the region
+          $(this).attr('aria-expanded', 'true');
+  
+          // move focus to the region
+          
+          $('#'+regionId).slideDown(function(){
+            $(this).removeAttr('hidden').focus();
+          });
+  
+          // update the button label
+          //thisObj.$id.find('span').html('Hide');
+  
+        }
+        else { // region is expanded
+  
+          // update the aria-expanded attribute of the region
+          $(this).attr('aria-expanded', 'false').focus();
+
+          $('#'+regionId).slideUp(function(){
+            $(this).attr('hidden','hidden');
+          });
+  
+          // update the button label
+          //thisObj.$id.find('span').html('Show');
+        }
+    });
+
+    // internal close button click handler
+    $('.notice-expand__close').click(function() {
+      
+      var regionId = $(this).attr('aria-controls');
+      $('.expand-control[aria-controls="'+regionId+'"]').trigger('click');
+
+    });
+
+});
